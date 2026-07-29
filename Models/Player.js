b9a50cc7bf17ef9155b04811845505e7fde37fb9
ERINA25423 <07erina18@gmail.com>
2026-07-29 10:01:16 +0900

    Update Player.js

--- Models/Player.js
+++ Models/Player.js
@@ -25,5 +25,80 @@ class Player {
         this.position = position;
 
     }
+    // ==================================================
+    // Getter
+    // ==================================================
+
+    getId() {
+
+        return this.id;
+
+    }
+
+    getNumber() {
+
+        return this.number;
+
+    }
+
+    getName() {
+
+        return this.name;
+
+    }
+
+    getPosition() {
+
+        return this.position;
+
+    }
+
+    // ==================================================
+    // Setter
+    // ==================================================
+
+    setNumber(number) {
+
+        this.number = number;
+
+    }
+
+    setName(name) {
+
+        this.name = name;
+
+    }
+
+    setPosition(position) {
+
+        this.position = position;
+
+    }
+
+    // ==================================================
+    // JSON
+    // ==================================================
+
+    toJSON() {
+
+        return {
+
+            id: this.id,
+
+            number: this.number,
+
+            name: this.name,
+
+            position: this.position
+
+        };
+
+    }
+
+    static fromJSON(data) {
+
+        return new Player(data);
+
+    }
 
 }
