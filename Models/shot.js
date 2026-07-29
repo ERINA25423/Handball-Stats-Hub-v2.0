ab648c274d715ab2454009f2e38ceb4665cd7d9d
ERINA25423 <07erina18@gmail.com>
2026-07-29 10:13:57 +0900

    Update shot.js

--- Models/shot.js
+++ Models/shot.js
@@ -2,6 +2,8 @@
 // Shot.js
 // ======================================================
 
+"use strict";
+
 class Shot {
 
     constructor({
@@ -19,6 +21,82 @@ class Shot {
         this.type = type;
 
         this.result = result;
+
+    }
+
+    // ==================================================
+    // Getter
+    // ==================================================
+
+    getCourse() {
+
+        return this.course;
+
+    }
+
+    getType() {
+
+        return this.type;
+
+    }
+
+    getResult() {
+
+        return this.result;
+
+    }
+
+    // ==================================================
+    // Setter
+    // ==================================================
+
+    setCourse(course) {
+
+        this.course = course;
+
+    }
+
+    setType(type) {
+
+        this.type = type;
+
+    }
+
+    setResult(result) {
+
+        this.result = result;
+
+    }
+
+    // ==================================================
+    // JSON
+    // ==================================================
+
+    toJSON() {
+
+        return {
+
+            course: this.course,
+
+            type: this.type,
+
+            result: this.result
+
+        };
+
+    }
+
+    static fromJSON(data) {
+
+        return new Shot({
+
+            course: data.course,
+
+            type: data.type,
+
+            result: data.result
+
+        });
 
     }
 
