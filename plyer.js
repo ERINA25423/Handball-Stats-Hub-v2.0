96622f54b4a6c59a996f99b32b956fa640c9f4d7
ERINA25423 <07erina18@gmail.com>
2026-07-29 09:33:03 +0900

    Create plyer.js

---
+++ plyer.js
@@ -0,0 +1,29 @@
+// ======================================================
+// Player.js
+// ======================================================
+
+class Player {
+
+    constructor({
+
+        id,
+
+        number,
+
+        name = "",
+
+        position = ""
+
+    }) {
+
+        this.id = id;
+
+        this.number = number;
+
+        this.name = name;
+
+        this.position = position;
+
+    }
+
+}
