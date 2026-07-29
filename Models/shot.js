a9ae6fcf7e4e9f1235273b5d3c3290afc5d635c9
ERINA25423 <07erina18@gmail.com>
2026-07-29 09:35:00 +0900

    Create shot.js

---
+++ Models/shot.js
@@ -0,0 +1,25 @@
+// ======================================================
+// Shot.js
+// ======================================================
+
+class Shot {
+
+    constructor({
+
+        course,
+
+        type,
+
+        result
+
+    }) {
+
+        this.course = course;
+
+        this.type = type;
+
+        this.result = result;
+
+    }
+
+}
